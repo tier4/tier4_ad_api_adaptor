a79b2dc11e514521c1bc86dcc2b32b1dfe5e5e3d
AhmedEbrahim <ahmed.a.d.ebrahim@gmail.com>
2023-03-21 16:35:14 +0200

    feat: handling invalid lanelet in RTC
    Signed-off-by: AhmedEbrahim <ahmed.a.d.ebrahim@gmail.com>

--- autoware_iv_external_api_adaptor/src/rtc_controller.hpp
+++ autoware_iv_external_api_adaptor/src/rtc_controller.hpp
@@ -85,6 +85,7 @@ private:
   std::unique_ptr<RTCModule> avoidance_by_lc_right_;
   std::unique_ptr<RTCModule> goal_planner_;
   std::unique_ptr<RTCModule> pull_out_;
+  std::unique_ptr<RTCModule> invalid_lanelet_;
 
   /* publishers */
   rclcpp::Publisher<CooperateStatusArray>::SharedPtr rtc_status_pub_;
