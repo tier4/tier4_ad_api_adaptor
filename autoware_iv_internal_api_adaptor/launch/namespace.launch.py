5dcd60a77645da672c5b8df74b773a73e82051a8
Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
2025-11-10 16:09:58 +0900

    feat: refactor launch files to use load_composable_node and add namespace management
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>

---
+++ autoware_iv_internal_api_adaptor/launch/namespace.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch.actions import SetLaunchConfiguration
+from launch.actions import OpaqueFunction
+
+def current_ros_namespace(context):
+    namespace = context.launch_configurations.get("ros_namespace", "")
+    return [SetLaunchConfiguration("current_ros_namespace", namespace)]
+
+def generate_launch_description():
+    return LaunchDescription([OpaqueFunction(function=current_ros_namespace)])
