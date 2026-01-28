f90c08f3d7b668ae358fab437cc1241d38ff7a66
tera24 <toshifumi.teranishi@tier4.jp>
2026-01-28 11:28:17 +0900

    feat: add hazard status relay and refactor launch files (#200)
    * feat: refactor launch files to use composable nodes and add scripts to handle namespace (fixed) (#187)
    
    * feat: refactor launch files to use composable nodes and add scripts to handle namespace (#183)
    
    * feat: refactor launch files to use composable nodes and add scripts to handle namespace
    
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    
    * fix: restore a removed node
    
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    
    * style: add copyright to pass linter
    
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    
    ---------
    
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    Co-authored-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    
    * fix: use multithread to avoid dead lock of relayed service
    
    ---------
    
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    Co-authored-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    
    * feat: add hazard status relay (#193)
    
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    
    ---------
    
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    Co-authored-by: Takahisa Ishikawa <interimadd@gmail.com>
    Co-authored-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
    Co-authored-by: Takagi, Isamu <43976882+isamu-takagi@users.noreply.github.com>

---
+++ tier4_deprecated_api_adapter/src/hazard_status.cpp
@@ -0,0 +1,45 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "hazard_status.hpp"
+
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+namespace tier4_deprecated_api_adapter
+{
+
+HazardStatus::HazardStatus(const rclcpp::NodeOptions & options) : Node("hazard_status", options)
+{
+  const auto on_message = [this](const InternalMessage & internal) {
+    ExternalMessage external;
+    external.stamp = internal.stamp;
+    external.status.level = internal.status.level;
+    external.status.emergency = internal.status.emergency;
+    external.status.emergency_holding = internal.status.emergency_holding;
+    external.status.diag_no_fault = internal.status.diag_no_fault;
+    external.status.diag_safe_fault = internal.status.diag_safe_fault;
+    external.status.diag_latent_fault = internal.status.diag_latent_fault;
+    external.status.diag_single_point_fault = internal.status.diag_single_point_fault;
+    pub_->publish(external);
+  };
+  pub_ = create_publisher<ExternalMessage>("/api/external/get/hazard_status", 1);
+  sub_ = create_subscription<InternalMessage>("/system/emergency/hazard_status", 1, on_message);
+}
+
+}  // namespace tier4_deprecated_api_adapter
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(tier4_deprecated_api_adapter::HazardStatus)
