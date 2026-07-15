ba701180cadfe1d5528d6f52dcd4a3fe72f6656e
Takagi, Isamu <isamu.takagi@tier4.jp>
2026-07-15 17:26:57 +0900

    add interface
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- tier4_monitoring/src/monitoring.hpp
+++ tier4_monitoring/src/monitoring.hpp
@@ -17,6 +17,10 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <tier4_external_api_msgs/msg/monitoring_heartbeat.hpp>
+#include <tier4_external_api_msgs/msg/monitoring_status.hpp>
+#include <tier4_external_api_msgs/srv/change_monitoring_mode.hpp>
+
 namespace tier4_monitoring
 {
 
@@ -26,6 +30,19 @@ public:
   explicit Monitoring(const rclcpp::NodeOptions & options);
 
 private:
+  using MonitoringStatus = tier4_external_api_msgs::msg::MonitoringStatus;
+  using MonitoringHeartbeat = tier4_external_api_msgs::msg::MonitoringHeartbeat;
+  using ResponseStatus = tier4_external_api_msgs::msg::ResponseStatus;
+  using ChangeMonitoringMode = tier4_external_api_msgs::srv::ChangeMonitoringMode;
+
+  rclcpp::Publisher<MonitoringStatus>::SharedPtr pub_status_;
+  rclcpp::Subscription<MonitoringHeartbeat>::SharedPtr sub_heartbeat_;
+  rclcpp::Service<ChangeMonitoringMode>::SharedPtr srv_change_monitoring_mode_;
+
+  void on_heartbeat(const MonitoringHeartbeat::SharedPtr msg);
+  void on_change_monitoring_mode(
+    const ChangeMonitoringMode::Request::SharedPtr req,
+    const ChangeMonitoringMode::Response::SharedPtr res);
 };
 
 }  // namespace tier4_monitoring
