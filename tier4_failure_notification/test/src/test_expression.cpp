c5b31a7426d4dbf19c0172e050f0b3c9606cf99b
Takagi, Isamu <isamu.takagi@tier4.jp>
2026-08-19 11:51:34 +0900

    add test helper
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- tier4_failure_notification/test/src/test_expression.cpp
+++ tier4_failure_notification/test/src/test_expression.cpp
@@ -16,40 +16,58 @@
 
 #include <gtest/gtest.h>
 
+#include <string>
+#include <vector>
+
 using namespace autoware::failure_notification;  // NOLINT(build/namespaces)
+
+namespace autoware::failure_notification
+{
+bool operator==(const Expression & x, const Expression & y)
+{
+  return (x.data == y.data) && (x.args == y.args);
+}
+}  // namespace autoware::failure_notification
+
+Expression v(const std::string & name)
+{
+  return Expression{name, std::nullopt};
+}
+
+template <typename... Args>
+Expression f(const std::string & name, Args... args)
+{
+  return Expression{name, std::vector<Expression>{args...}};
+}
 
 TEST(Expression, Constant)
 {
   const auto expr = Expression::parse("constant");
-  EXPECT_EQ(expr.data, "constant");
-  EXPECT_FALSE(expr.args.has_value());
+  EXPECT_EQ(expr, v("constant"));
 }
 
 TEST(Expression, FunctionNoArgs)
 {
   const auto expr = Expression::parse("function()");
-  EXPECT_EQ(expr.data, "function");
-  EXPECT_TRUE(expr.args.has_value());
-  EXPECT_EQ(expr.args->size(), 0);
+  EXPECT_EQ(expr, f("function"));
 }
 
 TEST(Expression, FunctionOneArg)
 {
   const auto expr = Expression::parse("function(one)");
-  EXPECT_EQ(expr.data, "function");
-  EXPECT_TRUE(expr.args.has_value());
-  EXPECT_EQ(expr.args->size(), 1);
-  EXPECT_EQ(expr.args->at(0).data, "one");
+  EXPECT_EQ(expr, f("function", v("one")));
 }
 
 TEST(Expression, FunctionTwoArgs)
 {
   const auto expr = Expression::parse("function(one, two)");
-  EXPECT_EQ(expr.data, "function");
-  EXPECT_TRUE(expr.args.has_value());
-  EXPECT_EQ(expr.args->size(), 2);
-  EXPECT_EQ(expr.args->at(0).data, "one");
-  EXPECT_EQ(expr.args->at(1).data, "two");
+  EXPECT_EQ(expr, f("function", v("one"), v("two")));
+}
+
+TEST(Expression, FunctionNested)
+{
+  const auto expr = Expression::parse("function(A, B(C), D(E(F), G))");
+  EXPECT_EQ(expr, f("function", v("A"), f("B", v("C")), f("D", f("E", v("F")), v("G"))));
 }
 
 TEST(Expression, FunctionNoClose)
@@ -70,21 +88,4 @@ TEST(Expression, FunctionNoOpen)
 TEST(Expression, FunctionOverOpen)
 {
   EXPECT_THROW(Expression::parse("function((one, two)"), std::runtime_error);
-}
-
-TEST(Expression, FunctionNested)
-{
-  const auto expr = Expression::parse("function(one, two(), three(four, five))");
-  EXPECT_EQ(expr.data, "function");
-  EXPECT_TRUE(expr.args.has_value());
-  EXPECT_EQ(expr.args->size(), 3);
-  EXPECT_EQ(expr.args->at(0).data, "one");
-  EXPECT_EQ(expr.args->at(1).data, "two");
-  EXPECT_TRUE(expr.args->at(1).args.has_value());
-  EXPECT_EQ(expr.args->at(1).args->size(), 0);
-  EXPECT_EQ(expr.args->at(2).data, "three");
-  EXPECT_TRUE(expr.args->at(2).args.has_value());
-  EXPECT_EQ(expr.args->at(2).args->size(), 2);
-  EXPECT_EQ(expr.args->at(2).args->at(0).data, "four");
-  EXPECT_EQ(expr.args->at(2).args->at(1).data, "five");
 }
